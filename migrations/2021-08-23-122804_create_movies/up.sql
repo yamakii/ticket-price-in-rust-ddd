93eb193f7903411a983276aaf90ffd9010a25147
yamaki <yamaki@wh-plus.com>
2021-08-27 20:33:37 +0900

    マスターデータの初期データを追加

--- migrations/2021-08-23-122804_create_movies/up.sql
+++ migrations/2021-08-23-122804_create_movies/up.sql
@@ -4,3 +4,9 @@ CREATE TABLE movies
     title      TEXT    NOT NULL,
     base_price INTEGER NOT NULL
 );
+
+insert into movies (title, base_price)
+values
+    ('ステキな恋愛映画', 1000),
+    ('ものすごいSF映画', 1800),
+    ('おもしろいアニメ', 1200);
--- migrations/2021-08-23-235510_create_customer_type_discounts/up.sql
+++ migrations/2021-08-23-235510_create_customer_type_discounts/up.sql
@@ -3,3 +3,9 @@ CREATE TABLE customer_type_discounts
     customer_type INTEGER PRIMARY KEY,
     discount INTEGER NOT NULL
 );
+
+insert into customer_type_discounts
+values
+       (1, 0),
+       (2, 50),
+       (3, 30);
