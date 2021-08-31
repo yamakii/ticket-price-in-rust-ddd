0f7f89db7ab7bb29bed367c448cb223fefc97051
yamakii <hiroshi_yamaki@caddi.jp>
2021-08-31 22:33:16 +0900

    panicするunwrapとexpectを修正

--- src/order/infra/db/service/ticket_price.rs
+++ src/order/infra/db/service/ticket_price.rs
@@ -21,12 +21,17 @@ impl DbBasicPriceService {
 impl BasicPriceService for DbBasicPriceService {
     fn basic_price(&self, key: MovieId) -> TicketPrice {
         let key: u32 = key.into();
-        let conn = self.pool.get().unwrap();
+        let conn = match self.pool.get() {
+            Ok(c) => c,
+            _ => return 0.into()
+        };
 
-        let movie_dto: MovieDTO = movies
+        let movie_dto: MovieDTO = match movies
             .find(key as i32)
-            .first(&conn)
-            .expect("Error loading movies");
+            .first(&conn) {
+            Ok(m) => m,
+            _ => return 0.into()
+        };
 
         movie_dto.basic_price.into()
     }
@@ -45,12 +50,20 @@ impl DbCustomerTypeDiscountService {
 impl CustomerTypeDiscountService for DbCustomerTypeDiscountService {
     fn get(&self, customer_type: CustomerType) -> CustomerTypeDiscount {
         let key = customer_type as i32;
-        let conn = self.pool.get().unwrap();
+        let conn =
+            match self.pool.get() {
+                Ok(c) => c,
+                _ => return CustomerTypeDiscount::new(customer_type, 0)
+            };
 
-        let customer_type_discount_dto: CustomerTypeDiscountDTO = customer_type_discounts
-            .find(key as i32)
-            .first(&conn)
-            .expect("Error loading customer_type_discounts");
+        let customer_type_discount_dto: CustomerTypeDiscountDTO =
+            match customer_type_discounts
+                .find(key as i32)
+                .first(&conn)
+            {
+                Ok(c) => c,
+                _ => return CustomerTypeDiscount::new(customer_type, 0)
+            };
 
         CustomerTypeDiscount::new(customer_type, customer_type_discount_dto.discount as u32)
     }
